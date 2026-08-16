f2b85354f785daf933fb4211a137fbc590755b40
Deepak Kumar <kartikkum568@gmail.com>
2026-08-16 18:20:03 +0530

    Change of pattern

--- Dsa/Patterns/pattern2.cpp
+++ Dsa/Patterns/pattern2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void print(int n){
     for(int i=0; i<n; i++){
         for(int j=0; j<=i; j++){
-            cout<<"*";
+            cout<<"#";
 
         }
         cout<<endl;
