b2e0414da338c1d655cd5f856f2c811d24d3ba00
Deepak Kumar <kartikkum568@gmail.com>
2026-08-22 00:47:31 +0530

    Vector Iterators

--- STL/vector3.cpp
+++ STL/vector3.cpp
@@ -6,7 +6,7 @@ int main (){
    // v.erase(v.begin());  //Delete the element at first
    // v.erase(v.begin()+ 2);// Going to the next element
    // v.erase(v.begin()+2, v.begin() + 4); // Delete element form 2 to 3 position
-   //v.insert(v.begin() + 2, 100); // Inster element 100 in position 2
+   // v.insert(v.begin() + 2, 100); // Inster element 100 in position 2
    v.clear();
     for(int print : v){
         cout<<" "<<print;
@@ -14,5 +14,5 @@ int main (){
     cout<<endl;
     cout<<"Vector size: " << v.size() << "Vector capacity : "<< v.capacity()<<endl;
     cout<<"Is Vector empty"<<v.empty()<<endl;
-    return 0;.
+    return 0;
 }
